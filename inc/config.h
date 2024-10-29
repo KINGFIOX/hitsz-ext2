716d322f426412bc009260597fe5efa84a16454b
wangfiox <wangfiox@localhost.localdomain>
2024-10-29 09:31:08 +0800

    WIP xv6fs

---
+++ inc/config.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define PACKAGE "fuse-ext2"
+
+#define VERSION "0.0.11"
+
+#define FUSE_USE_VERSION 27
--- src/main.cxx
+++ src/main.cxx
@@ -1,11 +1,62 @@
+#include "config.h"
+
 extern "C" {
 #include <fuse.h>
 }
 
+#include <cstdlib>
 #include <iostream>
 
-int main(int argc, char** argv) {
-  std::cout << "Hello, World!" << std::endl;
-  // Return success
+#include "limits.h"
+
+static const struct fuse_operations xv6fs_ops = {
+    //     .getattr = op_getattr,
+    //     .readlink = op_readlink,
+    //     .mknod = op_mknod,
+    //     .mkdir = op_mkdir,
+    //     .unlink = op_unlink,
+    //     .rmdir = op_rmdir,
+    //     .symlink = op_symlink,
+    //     .rename = op_rename,
+    //     .link = op_link,
+    //     .chmod = op_chmod,
+    //     .chown = op_chown,
+    //     .truncate = op_truncate,
+    //     .open = op_open,
+    //     .read = op_read,
+    //     .write = op_write,
+    //     .statfs = op_statfs,
+    //     .flush = op_flush,
+    //     .release = op_release,
+    //     .fsync = op_fsync,
+    //     .setxattr = NULL,
+    //     .getxattr = op_getxattr,
+    //     .listxattr = NULL,
+    //     .removexattr = NULL,
+    //     .opendir = op_open,
+    //     .readdir = op_readdir,
+    //     .releasedir = op_release,
+    //     .fsyncdir = op_fsync,
+    //     .init = op_init,
+    //     .destroy = op_destroy,
+    //     .access = op_access,
+    //     .create = op_create,
+    //     .ftruncate = op_ftruncate,
+    //     .fgetattr = op_fgetattr,
+    //     .lock = NULL,
+    //     .utimens = op_utimens,
+    //     .bmap = NULL,
+};
+
+// /// ./xv6fs log.txt ./fs.img ./mnt
+int main(int argc, char *argv[]) {
+  if (argc != 3) {
+    std::cerr << "Usage: " << argv[0] << "<./log.txt> <./fs.img> <./mnt>" << std::endl;
+    return 1;
+  }
+  //   char* log = argv[1];
+  //   char* img = argv[2];
+  //   char* mnt = argv[3];
+  fuse_main(argc, argv, &xv6fs_ops, nullptr);
   return 0;
 }
